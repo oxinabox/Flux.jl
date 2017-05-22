f8c8e82544bf6a210fe9fd57f0a27febb484aead
Mike J Innes <mike.j.innes@gmail.com>
2017-05-22 16:18:41 +0100

    obviate mapconst

--- src/compiler/code.jl
+++ src/compiler/code.jl
@@ -1,4 +1,4 @@
-import DataFlow: mapconst, cse
+import DataFlow: cse
 using MacroTools: @q
 
 export @net
@@ -6,7 +6,7 @@ export @net
 function graphdef(ex, params = [])
   @capture(shortdef(ex), (args__,) -> body_)
   body = @> body MacroTools.flatten liftloops graphm DataFlow.il
-  body = mapconst(x -> x in params ? :(self.$x) : x, body)
+  body = map(x -> x in params ? :(self.$x) : x, body)
   return args, body
 end
 
@@ -52,7 +52,7 @@ end
 
 import Lazy: groupby
 
-reifyparams(v::IVertex) = mapconst(x -> x isa Param ? x.x : x, v)
+reifyparams(v::IVertex) = map(x -> x isa Param ? x.x : x, v)
 
 # TODO: type hints for parameters
 
@@ -68,14 +68,14 @@ function process_type(ex)
     $(build_type(T, params))
     $(esc(:((self::$T)($(args...)) = $(build_forward(body, args)))))
     $(esc(:(Flux.update!(self::$T, η)))) = ($(map(p -> :(update!($self.$p, η)), pnames)...);)
-    $(esc(:(Flux.graph(self::$T)))) = $(DataFlow.constructor(mapconst(esc, makegraph(body, args))))
+    $(esc(:(Flux.graph(self::$T)))) = $(DataFlow.constructor(map(esc, makegraph(body, args))))
     nothing
   end
 end
 
 function process_anon(ex)
   args, body = graphdef(ex)
-  :(Capacitor($(DataFlow.constructor(mapconst(esc, makegraph(body, args)[1])))))
+  :(Capacitor($(DataFlow.constructor(map(esc, makegraph(body, args)[1])))))
 end
 
 function process_def(ex)
